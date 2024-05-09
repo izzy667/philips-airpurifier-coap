b1c446a096ffac216143109a1cfd668cd130c5ed
izzy667 <izzy667@users.noreply.github.com>
2024-05-09 09:46:48 +0200

    __init__.py: changes reverted

--- custom_components/philips_airpurifier_coap/__init__.py
+++ custom_components/philips_airpurifier_coap/__init__.py
@@ -130,8 +130,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     try:
         future_client = CoAPClient.create(host)
-        _LOGGER.error("trying to connect to host: %s", host)
-        client = await asyncio.wait_for(future_client, timeout=None)
+        client = await asyncio.wait_for(future_client, timeout=25)
         _LOGGER.debug("got a valid client for host %s", host)
     except Exception as ex:
         _LOGGER.warning(r"Failed to connect to host %s: %s", host, ex)
